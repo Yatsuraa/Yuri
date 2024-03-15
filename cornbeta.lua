d8c0650bdaf07de1b0f5863865b07b2c69133876
Yatsuraa <163134623+Yatsuraa@users.noreply.github.com>
2024-03-15 20:34:55 +0700

    Add files via upload

--- cornbeta.lua
+++ cornbeta.lua
@@ -1,14 +1,9 @@
 local Library = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
 
-local PhantomForcesWindow = Library:NewWindow("CORN HYBER 🌽")
+local Window = Library:NewWindow("CORN HYBER🌽")
+local Section = Window:NewSection("By Gaubaptuoi🌽")
 
-local AutoParry = PhantomForcesWindow:NewSection("Main")
-
-AutoParry:CreateButton("CORN HUB SPAM", function()getgenv().SpamSpeed = 1
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3ABlade%20Ball%20Spam",true))()
-end)
-
-Section:CreateToggle("Auto Parry V1", function(value)
+Section:CreateToggle("Auto Parry Beta1 🌽", function(value)
 getgenv().config = getgenv().config or {
     hit_time = 0.5, -- // recommended 0.25 to 0.75 \ --
  
@@ -22,16 +17,16 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/ma
 print(value)
 end)
 
-Section:CreateToggle("Auto Parry V2🌽", function(value)
+Section:CreateToggle("Auto Parry Beta2 🌽", function(value)
 loadstring(game:HttpGet(('https://scriptblox.com/raw/Blade-Ball-Opensource-autoparry-8123'),true))()
 print(value)
 end)
 
-Section:CreateDropdown("Parry Mod V🌽", {"TryHard", "Normal",}, 2, function(text)
+Section:CreateDropdown("Parry Mod 🌽", {"TryHard", "Normal",}, 2, function(text)
 print(text)
 end)
 
-Section:CreateButton("OP Manual Spam🌽", function()
+Section:CreateButton("OP Manual Spam", function()
 local gui, frame, button = Instance.new("ScreenGui", game.CoreGui), Instance.new("Frame"), Instance.new("TextButton")
 gui.ResetOnSpawn = false
 frame.Size, frame.Position, frame.BackgroundColor3, frame.BorderSizePixel, frame.Active, frame.Draggable, frame.Parent = UDim2.new(0, 150, 0, 75), UDim2.new(0, 10, 0, 10), Color3.new(0, 0, 0), 0, true, true, gui
@@ -51,12 +46,41 @@ local function toggle()
 end
 
 local function showNotification()
-    game.StarterGui:SetCore("SendNotification", {Title = "Manual Spam", Text = "By Gaubaptuoi", Duration = 5})
+    game.StarterGui:SetCore("SendNotification", {Title = "Manual Spam", Text = "Made By SumitScripts", Duration = 5})
 end
 
 button.MouseButton1Click:Connect(function()
     toggle()
     showNotification()
 end)
+print("Clicked")
+end)
+
+Section:CreateButton("Fps Booster", function()
+-- Made by RIP#6666
+_G.Settings = {
+    Players = {
+        ["Ignore Me"] = true, -- Ignore your Character
+        ["Ignore Others"] = true -- Ignore other Characters
+    },
+    Meshes = {
+        Destroy = false, -- Destroy Meshes
+        LowDetail = true -- Low detail meshes (NOT SURE IT DOES ANYTHING)
+    },
+    Images = {
+        Invisible = true, -- Invisible Images
+        LowDetail = false, -- Low detail images (NOT SURE IT DOES ANYTHING)
+        Destroy = false, -- Destroy Images
+    },
+    ["No Particles"] = true, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparkles
+    ["No Camera Effects"] = true, -- Disables all PostEffect's (Camera/Lighting Effects)
+    ["No Explosions"] = true, -- Makes Explosion's invisible
+    ["No Clothes"] = true, -- Removes Clothing from the game
+    ["Low Water Graphics"] = true, -- Removes Water Quality
+    ["No Shadows"] = true, -- Remove Shadows
+    ["Low Rendering"] = true, -- Lower Rendering
+    ["Low Quality Parts"] = true -- Lower quality parts
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/FPSBooster.lua"))()
 print("Clicked")
 end)
